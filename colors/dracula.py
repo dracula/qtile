4187bbb6ad64def35ada206773f07b2b3bc56e2c
ALipAkrohk <rhpcir@gmail.com>
2023-03-11 01:36:16 +0330

    Correcting the system of using colors. Modifying the calling system and removing additional parts. Modifying the software auto-start system.

--- colors/dracula.py
+++ colors/dracula.py
@@ -1,7 +1,7 @@
 def Dracula():
     return {
-        "Background": "#282a36",
-        "Foreground": "#f8f8f2",
+        "background": "#282a36",
+        "foreground": "#f8f8f2",
         "Current": "#44475a",
         "Comment": "#6272a4",
         "Cyan": "#8be9fd",
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 #  qtile built-in libraries
+import subprocess
+
 from libqtile import bar, hook
 from libqtile.config import Screen
-from libqtile.lazy import lazy
 
 # Config files and other libraries
 from cfg.bindings import init_app_run, init_keys
@@ -9,6 +10,23 @@ from cfg.groups import init_group_mappings, init_groups
 from cfg.layouts import init_layouts
 from cfg.widgets import init_widgets
 from colors.dracula import Dracula
+
+
+# Set up the hooks
+@hook.subscribe.startup_once
+def autostart():
+    subprocess.Popen(["picom -b"])
+    subprocess.Popen(["feh --bg-max --randomize ~/.config/qtile/wallpapers/*"])
+
+
+# Define the bar
+def init_bar():
+    return bar.Bar(
+        widgets,
+        24,
+        opacity=0.66,
+    )
+
 
 # Set the Vars objects
 keys = init_keys()
@@ -19,11 +37,10 @@ layouts = init_layouts()
 groups = init_groups()
 group_mappings = init_group_mappings()
 
-
 # Set your default widget styles
 colors = Dracula()
 widget_defaults = dict(
-    font="Fera Code Font Mono",
+    font="Fira Code Nerd Font",
     fontsize=12,
     fontshadow=None,
 )
@@ -31,32 +48,5 @@ widget_defaults = dict(
 # Merge the theme dictionary with the widget_defaults dictionary
 widget_defaults.update(colors)
 
-
-# Set up the hooks
-@hook.subscribe.startup_once
-def autostart():
-    lazy.spawn("picom -b")
-
-
-# Define the bar
-def init_bar():
-    return bar.Bar(
-        widgets,
-        24,
-    )
-
-
 # Set up the screens
 screens = [Screen(top=init_bar())]
-
-# Set the remaining attributes of the qtile object
-def main(qtile):
-    qtile.configured = False
-    # Set the remaining attributes of the qtile object
-    qtile.widget_defaults = widget_defaults
-    qtile.keys = keys
-    qtile.groups = groups
-    qtile.group_mappings = group_mappings
-    qtile.layouts = layouts
-    qtile.screen = screens
-    qtile.configured = True
